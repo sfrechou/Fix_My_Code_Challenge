df9afd6ad291c41a78d30840ad05413b0d204041
sfrechou <solefrechou@hotmail.com>
2021-05-23 23:54:21 +0000

    Fix square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,32 +1,50 @@
 #!/usr/bin/python3
 """
-Module that defines a square
+Module that defines a Square
 """
 
 
-class Square():
-    """Defines a square"""
+class Square:
+    """Class that defines a Square"""
+
     width = 0
 
-    def __init__(self, *args, **kwargs):
-        """Instantiates a Square"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if type(value) is int and value >= 0:
-                    setattr(self, key, value)
-        else:
-            self.width = width
+    def __init__(self, width=0):
+        """Instantiation attributes"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def width(self):
+        """Property to retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter to set width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     def area(self):
-        """Area of a square"""
-        return self.width ** 2
+        """Calculates the area of a Rectangle"""
+        return self.__width * self.__width
 
     def perimeter(self):
-        """Perimeter of a square"""
-        return self.width * 4
+        """Calculates the permiter of a rectangle"""
+        if self.__width == 0:
+            return 0
+        else:
+            return (self.__width * 4)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.width)
+        """Prints therep of a Square"""
+        return "{}/{}".format(self.__width, self.__width)
 
 if __name__ == "__main__":
     s = Square(width=12)
