b4780b0119367aad2e7d69fe18de861397717814
sfrechou <solefrechou@hotmail.com>
2021-05-23 23:57:34 +0000

    Fix square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -9,13 +9,18 @@ class Square:
 
     width = 0
 
-    def __init__(self, width=0):
+    def __init__(self, width=0, height=0):
         """Instantiation attributes"""
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -31,23 +36,37 @@ class Square:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        """Property to retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter to set height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
     def area(self):
         """Calculates the area of a Rectangle"""
-        return self.__width * self.__width
+        return self.__width * self.__height
 
     def perimeter(self):
         """Calculates the permiter of a rectangle"""
-        if self.__width == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
-            return (self.__width * 4)
+            return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
         """Prints therep of a Square"""
-        return "{}/{}".format(self.__width, self.__width)
+        return "{}/{}".format(self.__width, self.__height)
 
 if __name__ == "__main__":
-    s = Square(width=12)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area())
     print(s.perimeter())
