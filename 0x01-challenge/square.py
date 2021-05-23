69eb10a013adbf88f34938b166adb9a842e26b97
sfrechou <solefrechou@hotmail.com>
2021-05-23 23:46:20 +0000

    Fix square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,64 +1,35 @@
 #!/usr/bin/python3
 """
-documentation
+Module that defines a square
 """
 
 
-class Square:
-    ''' Square class '''
-
+class Square():
+    """Defines a square"""
     width = 0
-    height = 0
 
-    def __init__(self, width, height):
-        ''' initializer for attributes '''
-        self.width = width
-        self.height = height
+    def __init__(self, *args, **kwargs):
+        """Instantiates a Square"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if type(value) is int and value >= 0:
+                    setattr(self, key, value)
+        else:
+            self.width = width
 
-    def __wh_validator(self, name, value):
-        ''' comments '''
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be > 0".format(name))
+    def area(self):
+        """Area of a square"""
+        return self.width ** 2
 
-    @property
-    def width(self):
-        ''' comments '''
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        ''' comments '''
-        self.__wh_validator("width", value)
-        self.__width = value
-
-    @property
-    def height(self):
-        ''' comments '''
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        ''' comments '''
-        self.__wh_validator("height", value)
-        self.__height = value
-
-    def area_of_my_square(self):
-        """ Area of the square calculates area """
-        return self.width * self.height
-
-    def permiter_of_my_square(self):
-        ''' Perimeter of Square '''
-        return (self.width * 2) + (self.height * 2)
+    def perimeter(self):
+        """Perimeter of a square"""
+        return self.width * 4
 
     def __str__(self):
-        ''' str representation of instance '''
-        return "{}/{}".format(self.width, self.height)
-
+        return "{}/{}".format(self.width, self.width)
 
 if __name__ == "__main__":
-    s = Square(width=12, height=12)
+    s = Square(width=12)
     print(s)
-    print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.area())
+    print(s.perimeter())
