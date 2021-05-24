c416191776538930430383f8f56ea085c8ecdc06
sfrechou <solefrechou@hotmail.com>
2021-05-24 00:10:16 +0000

    Fix square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,73 +1,33 @@
 #!/usr/bin/python3
 """
-Module that defines a Square
+Modules that defines a Square
 """
 
 
-class Square:
+class Square():
     """Class that defines a Square"""
-
     width = 0
     height = 0
 
-    def __init__(self, width=0, height=0):
-        """Instantiation attributes"""
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.width = width
-        self.height = height
-
-    @property
-    def width(self):
-        """Property to retrieve width"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Setter to set width"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Property to retrieve height"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Setter to set height"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+    def __init__(self, *args, **kwargs):
+        """Instantiates a Square"""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area(self):
-        """Calculates the area of a Rectangle"""
+        """Area of a square"""
         return self.width * self.height
 
-    def perimeter(self):
-        """Calculates the permiter of a rectangle"""
-        if self.width == 0 or self.height == 0:
-            return 0
-        else:
-            return (self.width * 2) + (self.height * 2)
+    def permiter(self):
+        """Perimeter of a square"""
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """Prints therep of a Square"""
+        """String rep of a square"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-    s = Square(width=9, height=9)
+    s = Square(width=12, height=12)
     print(s)
     print(s.area())
-    print(s.perimeter())
+    print(s.permiter())
